4a8992c564736c8da91def941b288160f95bc050
mosafdar <mosafdaralii@gmail.com>
2025-01-01 16:36:42 +0500

    add tracker app and configure url routing

---
+++ tracker/trackerapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'trackerapp'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('add/', views.add_food, name='add_food'),
+    # path('delete/<int:food_id>/', views.delete_food, name='delete_food'),
+    # path('update/<int:food_id>/', views.update_food, name='update_food'),
+]
